e3273eddfbab1a2bf42544e5d14474ce4b99ce7c
GilmarCQ <gacampanaq@gmail.com>
2022-03-24 19:58:18 -0500

    Limpieza semana 04

--- dtos/paginacion_dto.py
+++
@@ -1,7 +0,0 @@
-from config import validador
-from marshmallow import fields
-
-
-class PaginacionRequestDTO(validador.Schema):
-    page = fields.Integer(required=False, load_default=1, min=1)
-    perPage = fields.Integer(required=False, load_default=10, min=1)
